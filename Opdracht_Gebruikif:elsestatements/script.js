55333d70505457cf47e6dce60f91cd6ad7c36c20
Yasmin <88096861+yasminevans@users.noreply.github.com>
2021-08-03 09:55:11 +0200

    Add files via upload

---
+++ Opdracht_Gebruikif:elsestatements/script.js
@@ -0,0 +1,24 @@
+let myAge = 18;
+
+if (myAge > 17) {
+    console.log("Je mag naar binnen")
+}
+else {
+    console.log("Je mag niet naar binnen")
+}
+
+let isFemale = true;
+if (isFemale) {
+    console.log("Je bent vrouw")
+}
+else {
+    console.log("Helaas, je bent van het mindere geslacht")
+}
+
+let driverStatus = "bob";
+if (driverStatus == "bob") {
+    console.log("Je mag naar huis")
+}
+else {
+    console.log("Je blijft hier slapen")
+}
